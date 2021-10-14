ebd183c57e2af6d180f2755893e705e801a59f4a
Guilherme Silveira <silveira.alves.guilherme@gmail.com>
2021-10-14 17:41:07 -0300

    Definição da Model de Curso

--- Models/Curso.cs
+++ Models/Curso.cs
@@ -5,8 +5,21 @@ using System.Threading.Tasks;
 
 namespace DindinAPI.Models
 {
+    public class Aula
+    {
+        public int AulaId { get; set; }
+        public string AulaDescricao { get; set; }
+        public string AulaLink { get; set; }
+        public int CursoId { get; set; }
+        public Curso Curso { get; set; }
+    }
     public class Curso
     {
-
+        public int CursoId { get; set; }
+        public string Titulo { get; set; }
+        public string Professor { get; set; }
+        public string ImagemCapa { get; set; }
+        public string CursoDescricao { get; set; }
+        public ICollection<Aula> Aula { get; set; } 
     }
 }
