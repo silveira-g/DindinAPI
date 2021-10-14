f9951b88ecc57f18399d2cf6ff6b678bf1c8634e
Guilherme Silveira <silveira.alves.guilherme@gmail.com>
2021-10-14 17:59:15 -0300

    Definindo ConnectionString do Banco de Dados e primeira Migration

---
+++ Migrations/20211014205423_Inicial.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DindinAPI.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cursos",
+                columns: table => new
+                {
+                    CursoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Professor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImagemCapa = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CursoDescricao = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cursos", x => x.CursoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Aulas",
+                columns: table => new
+                {
+                    AulaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AulaDescricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AulaLink = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CursoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aulas", x => x.AulaId);
+                    table.ForeignKey(
+                        name: "FK_Aulas_Cursos_CursoId",
+                        column: x => x.CursoId,
+                        principalTable: "Cursos",
+                        principalColumn: "CursoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Aulas_CursoId",
+                table: "Aulas",
+                column: "CursoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aulas");
+
+            migrationBuilder.DropTable(
+                name: "Cursos");
+        }
+    }
+}
