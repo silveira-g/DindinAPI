0e7e44560b3f6cd61ab8978344defa298b630e8b
Guilherme Silveira <silveira.alves.guilherme@gmail.com>
2021-10-14 17:46:55 -0300

    Definição do DbContext

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DindinAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DindinAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Aula> Aulas { get; set; }
+    }
+}
--- Models/Curso.cs
+++ Models/Curso.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,18 +9,26 @@ namespace DindinAPI.Models
     public class Aula
     {
         public int AulaId { get; set; }
+        [Required]
         public string AulaDescricao { get; set; }
+        [Required]
         public string AulaLink { get; set; }
+
         public int CursoId { get; set; }
         public Curso Curso { get; set; }
     }
     public class Curso
     {
         public int CursoId { get; set; }
+        [Required]
         public string Titulo { get; set; }
+        [Required]
         public string Professor { get; set; }
+        [Required]
         public string ImagemCapa { get; set; }
+        [Required]
         public string CursoDescricao { get; set; }
+        [Required]
         public ICollection<Aula> Aula { get; set; } 
     }
 }
