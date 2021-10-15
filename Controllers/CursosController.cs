be8a257d44096d6da89b9b4f1097a33b8a743ca6
Guilherme Silveira <silveira.alves.guilherme@gmail.com>
2021-10-15 20:55:28 -0300

    Criacao controller da entidade Cursos

--- Controllers/CursosController.cs
+++ Controllers/CursosController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DindinAPI.Models;
 using DindinAPI.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -18,6 +19,101 @@ namespace DindinAPI.Controllers
         {
             _cursoService = cursoService;
         }
+
+        [HttpGet]
+        public async Task<ActionResult<IAsyncEnumerable<Curso>>> GetCursos()
+        {
+            try
+            {
+                var cursos = await _cursoService.GetCursos();
+                return Ok(cursos);
+            }
+            catch
+            {
+
+                return BadRequest("Request inválido");
+            }
+        }
+
+        [HttpGet("{id:int}", Name ="GetCurso")]
+        public async Task<ActionResult<Curso>> GetCurso
+            (int id)
+        {
+            try
+            {
+                var curso = await _cursoService.GetCurso(id);
+                if (curso == null)
+                    return NotFound($"Não existe curso com id= {id}");
+                return Ok(curso);
+            }
+            catch
+            {
+
+                return BadRequest("Request inválido");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Curso curso)
+        {
+            try
+            {
+                await _cursoService.CreateCurso(curso);
+                return CreatedAtRoute(nameof(GetCurso), new { id = curso.CursoId }, curso);
+            }
+            catch
+            {
+
+                return BadRequest("Request inválido");
+            }
+        } 
+
+        [HttpPut("{id: int}")]
+        public async Task<ActionResult> Edit(int id, [FromBody] Curso curso)
+        {
+            try
+            {
+                if (curso.CursoId== id)
+                {
+                    await _cursoService.UpdateCurso(curso);
+                    return Ok($"Curso com id= {id} foi atualizado com sucesso");
+                }
+                else
+                {
+                    return BadRequest("Dados inconsistentes");
+                }
+            }
+            catch
+            {
+
+                return BadRequest("Request inválido");
+            }
+        }
+
+        [HttpDelete("{id: int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                var curso = await _cursoService.GetCurso(id);
+                if (curso != null)
+                {
+                    await _cursoService.DeleteCurso(curso);
+                    return Ok($"Curso de id= {id} removido com sucesso");
+                }
+                else
+                {
+                    return NotFound($"Aluno com id= {id} não localizado");
+                }
+            }
+            catch
+            {
+
+                return BadRequest("Request inválido");
+            }
+        }
+
+
 
     }
 }
