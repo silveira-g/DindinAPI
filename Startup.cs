d810184c0278b12e99e058545ed3499cbd05eb5b
Guilherme Silveira <silveira.alves.guilherme@gmail.com>
2021-10-18 22:15:56 -0300

    Edição das configurações do CORS

--- Startup.cs
+++ Startup.cs
@@ -31,7 +31,7 @@ namespace DindinAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors();
             services.AddDbContext<AppDbContext>(options =>   
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
